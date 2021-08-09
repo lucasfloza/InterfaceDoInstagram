e45bdc6443c5c397fa8d1beb7adee14beb936160
lucasfloza <78889763+lucasfloza@users.noreply.github.com>
2021-08-09 09:08:23 -0300

    Add files via upload

---
+++ recriando o instagram/js/script.js
@@ -0,0 +1,25 @@
+function login() {
+      
+
+   //excluindo elementos
+    
+    let item1 = document.querySelector("div.two");
+        
+        item1.style = "display:none;"                
+
+    let item2 = document.querySelector("div.oculto")
+
+        item2.style = "display:flex;"
+
+    let item3 = document.querySelector("div.cadastre-se")
+
+        item3.style = "display:flex;"
+
+    let item4 = document.querySelector("div.aplicativos")
+
+        item4.style = "display:flex;"    
+}
+    //criando elementos
+
+
+
